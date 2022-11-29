5af68f7486b21124cdb24898289d440cc7ecf0ae
Alec Benjamin Korotney <akorot@gl-login1.arc-ts.umich.edu>
2022-11-29 00:39:02 -0500

    bart stuff and more

--- job_train.sh
+++ job_train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --partition=gpu
-#SBATCH --time=00-06:00:00
-#SBATCH --gpus=2
+#SBATCH --time=00-01:00:00
+#SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=4
 #SBATCH --mem-per-gpu=64GB
 #SBATCH --account=eecs595f22_class
@@ -10,7 +10,7 @@
 # set up job
 module load python3.9-anaconda  
 # pushd essentially sets pwd
-pushd /home/musicer/595/Joke-Generator/
+pushd /home/akorot/Joke-Generator/
 source activate jokes
 
-python3 simple_joke_generator.py
+python3 simple_joke_generator.py --train_file datasets/data/reddit_preprocessed/unfunny.tsv --test_file datasets/data/reddit_preprocessed/test_unfunny.tsv --model_file model_unfunny_bart.torch
